8ca8385ae451c61b8bc42c626ad97c5a7a21f203
Gabi Emi <gabiemi2021@gmail.com>
2025-09-15 16:02:06 -0300

    feat: cria as queries de select

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,18 @@ CREATE TABLE artistas (
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, ano_lancamento, pais_origem, musica_famosa, ativo)
+VALUES
+    ('Adele', 'Solo', 1, 'Pop', 2006, 'Reino Unido', 'Rolling in the Deep', TRUE),
+    ('21 Pilots', 'Banda', 2, 'Rock Alternativo', 2009, 'EUA', 'Stressed Out', TRUE),
+    ('Coldplay', 'Banda', 4, 'Rock Alternativo', 2000, 'Reino Unido', 'Yellow', TRUE),
+    ('Beyoncé', 'Solo', 1, 'R&B/Pop', 2003, 'EUA', 'Crazy in Love', TRUE),
+    ('Ed Sheeran', 'Solo', 1, 'Pop/Folk', 2011, 'Reino Unido', 'Shape of You', TRUE),
+    ('Linkin Park', 'Banda', 6, 'Nu Metal/Rock', 2000, 'EUA', 'In the End', FALSE),
+    ('Shakira', 'Solo', 1, 'Pop Latino', 1995, 'Colômbia', 'Hips Don’t Lie', TRUE),
+    ('The Beatles', 'Banda', 4, 'Rock', 1960, 'Reino Unido', 'Hey Jude', FALSE),
+    ('Billie Eilish', 'Solo', 1, 'Pop Alternativo', 2016, 'EUA', 'Bad Guy', TRUE),
+    ('Rihanna', 'Solo', 1, 'Pop/R&B', 2005, 'Barbados', 'Umbrella', TRUE),
+    ('Queen', 'Banda', 4, 'Rock', 1973, 'Reino Unido', 'Bohemian Rhapsody', FALSE),
+    ('Eminem', 'Solo', 1, 'Hip-Hop', 1999, 'EUA', 'Lose Yourself', TRUE);
---
+++ queries/02-selects.sql
@@ -0,0 +1,7 @@
+SELECTS * FROM artistas;
+
+SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, genero_musical FROM artistas;
+WHERE id = 9 AND id = 3;
+
