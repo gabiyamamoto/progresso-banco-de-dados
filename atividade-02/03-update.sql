35846f83ee7dfa5c72a6848ee8f9764ca5043726
Gabi Emi <gabiemi2021@gmail.com>
2025-09-22 11:26:59 -0300

    feat: update produtos

---
+++ atividade-02/03-update.sql
@@ -0,0 +1,10 @@
+UPDATE produtos SET preco = preco - (preco * 20/100)
+WHERE categoria = 'skincare';
+
+UPDATE produtos SET estoque = estoque + 50;
+
+UPDATE produtos SET preco = preco + (preco * 15/100)
+WHERE marca = 'The ordinary'
+
+UPDATE produtos SET preco = preco - (preco * 10/100)
+WHERE estoque > 100;
