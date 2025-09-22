706a65e736c9df30446f7c01536f9f21fafc757a
Gabi Emi <gabiemi2021@gmail.com>
2025-09-22 11:28:36 -0300

    feat: ajustes no query

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -2,6 +2,5 @@ SELECT * FROM artistas;
 
 SELECT COUNT(*) AS total_artistas FROM artistas;
 
-SELECT nome, genero_musical FROM artistas;
+SELECT nome, genero_musical FROM artistas
 WHERE id = 9 AND id = 3;
-
