8bc0acc32334aa7dd1f932a4040d7b3a9252c7c2
Gabi Emi <gabiemi2021@gmail.com>
2025-09-22 09:23:30 -0300

    feat: enumera arquivos, cria primeiros deletes e updates, realiza contagem final

---
+++ queries/04-delete.sql
@@ -0,0 +1,12 @@
+DELETE FROM usuarios
+WHERE username = 'CheatCodeKing';
+
+SELECT * FROM usuarios;
+
+DELETE FROM usuarios
+WHERE id = 13;
+
+DELETE FROM usuarios
+WHERE pontos < 2000;
+
+SELECT COUNT(*) AS total_usuarios FROM usuarios;
