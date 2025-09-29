0b8ad97d7ea8911e30df87839e0b68e64df0718a
Gabi Emi <gabiemi2021@gmail.com>
2025-09-29 14:09:42 -0300

    feat: Correção dos deletes

--- atividade-02/04-delete.sql
+++ atividade-02/04-delete.sql
@@ -1,8 +1,14 @@
 DELETE FROM produtos
-WHERE estoque < 10;
+WHERE estoque < 200;
 
 DELETE FROM produtos
-WHERE preco < 200.00;
+WHERE preco < 100.00;
 
 DELETE FROM produtos
-WHERE estoque BETWEEN 1 AND 5;
+WHERE marca = 'Principia';
+
+DELETE FROM produtos
+WHERE estoque > 250 AND estoque < 400;
+
+DELETE FROM produtos
+WHERE estoque BETWEEN 250 AND 450;
