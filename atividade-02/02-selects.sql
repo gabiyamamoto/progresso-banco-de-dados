93993f80ed51ce31d122b7e08c5b706ffc4afe16
Gabi Emi <gabiemi2021@gmail.com>
2025-09-29 13:55:02 -0300

    feat: Correção do selects e updates da atividade 2

--- atividade-02/02-selects.sql
+++ atividade-02/02-selects.sql
@@ -1,4 +1,7 @@
 SELECT * FROM produtos;
 
+SELECT * FROM produtos
+WHERE marca = 'The ordinary';
+
 SELECT marca, COUNT(*) FROM produtos
 GROUP BY marca;
--- atividade-02/03-update.sql
+++ atividade-02/03-update.sql
@@ -4,10 +4,10 @@ WHERE categoria = 'skincare';
 UPDATE produtos SET estoque = estoque + 50;
 
 UPDATE produtos SET preco = preco + (preco * 15/100)
-WHERE marca = 'The ordinary'
+WHERE marca = 'The ordinary';
 
 UPDATE produtos SET preco = preco - (preco * 10/100)
-WHERE estoque > 100;
+WHERE estoque > 300;
 
 UPDATE produtos SET preco = preco - (preco * 30/100)
 WHERE categoria = 'perfumes';
